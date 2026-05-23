3eb203f65ea8d0e729c02df7fef1ef28c7a418a6
thinyurealestate-stack <thinyurealestate@gmail.com>
2026-05-24 00:37:16 +0630

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, request 
 import os
 
-app = Flask(__name+)
+app = Flask(*name+)
 
 PAGE_ACCESS_TOKEN = os.environ.get("PAGE_ACCESS_TOKEN")
 VERIFY_TOKEN = os.environ.get("VERIFY_TOKEN")
