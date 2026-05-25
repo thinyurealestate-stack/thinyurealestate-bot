3826c41b287b8c10a3f80bdb913912adf5b0a4a7
thinyurealestate-stack <thinyurealestate@gmail.com>
2026-05-25 20:08:17 +0630

    Update main.py

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ LISTINGS = {
                         🌸ပြင်ဆင်ပြီး အသင့်နေလို့ရ
                         🌸ဘိုးဘွားပိုင် အရပ်စာချုပ်
                         🌸ဂရန်ပြန်လျောက်လို့ရသောမြေ""",
-            "subtitle", "💸၇၀၀၀ သိန်း (ညှီနှိုင်း)",
+            "subtitle": "💸၇၀၀၀ သိန်း (ညှီနှိုင်း)",
             "image_url", "https://drive.google.com/uc?export=view&id=1rgLIGefRfvNGg-pU28d6NCnh6-wZKP8w"
         },
             {
